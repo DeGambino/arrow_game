4bfd9ecbd1bf809b1fc33009b5e4c70dab568eea
DeGambino <126100679+DeGambino@users.noreply.github.com>
2024-05-25 13:57:28 +0300

    Initialize project environment

---
+++ src/components/Playground/Playground.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+import { useAppDispatch, useAppSelector } from "../../app/hooks"
+
+import { setCurrentStep } from "./store/slices"
+
+import Controls from "./components/Controls"
+
+const Playground: React.FC = () => {
+  const state = useAppSelector(state => state.playground)
+  const dispatch = useAppDispatch()
+
+  const [isTimerActive, setIsTimerActive] = useState<boolean>(false)
+
+  return (
+    <div>
+      <Controls
+        isTimerActive={isTimerActive}
+        setIsTimerActive={setIsTimerActive}
+      />
+    </div>
+  )
+}
+
+export default Playground
